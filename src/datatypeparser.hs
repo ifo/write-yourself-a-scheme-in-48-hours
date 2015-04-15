c8fcfb5bf8cbe700916087d95b013c5cf94fcad1
Steve McCarthy <steve@redlua.com>
2015-04-15 14:14:00 -0400

    Chapter 2 through exercise 3

---
+++ src/datatypeparser.hs
@@ -0,0 +1,65 @@
+module Main where
+
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad (liftM)
+
+main :: IO ()
+main =
+  getArgs >>=
+  \args -> putStrLn (readExpr (args !! 0))
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right x  -> "Found value " ++ show x
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal =
+    Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+  deriving (Show)
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"" <|> oneOf "\\\"\n\r\t")
+  char '"'
+  return $ String x
+
+--escapeCharacters :: Parser Char
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = [first] ++ rest
+  return $ case atom of
+              "#t" -> Bool True
+              "#f" -> Bool False
+              _    -> Atom atom
+
+parseNumber :: Parser LispVal
+--parseNumber = liftM (Number . read) $ many1 digit
+{-
+parseNumber = do
+  x <- many1 digit
+  return $ (Number . read) x
+-}
+parseNumber =
+  many1 digit >>=
+  return . Number . read
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+            <|> parseString
+            <|> parseNumber
